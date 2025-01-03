2a4d2b7513c6df358048f253d45bfad96244229d
trevor-duane@github.com <trevorkayiira@gmail.com>
2025-01-03 10:50:24 +0300

    date fix

--- controllers/stock/Inventory.js
+++ controllers/stock/Inventory.js
@@ -35,12 +35,12 @@ export const updateInventoryItem = async (req, res) => {
 // Controller for adding inventory
 export const addInventory = async (req, res) => {
     try {
-        const { shopping_list_id, inventory_date, item_name, section, uom, quantity_recieved } = req.body;
+        const { shopping_list_id, item_name, section, uom, quantity_recieved } = req.body;
 
         // Assuming you have a model for Inventory
         const newInventoryItem = await Inventory.create({
             shopping_list_id,
-            inventory_date,
+            // inventory_date,
             item_name,
             section,
             uom,
