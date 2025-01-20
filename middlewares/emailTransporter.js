ac837c62b9e18604334b72930437b3b4b37acecf
trevor-duane@github.com <trevorkayiira@gmail.com>
2025-01-20 10:34:39 +0300

    emailbug fix

--- middlewares/emailTransporter.js
+++ middlewares/emailTransporter.js
@@ -6,7 +6,7 @@ dotenv.config();
 const transporter = nodemailer.createTransport({
     host: process.env.SMTP_HOST,
     port: process.env.SMTP_PORT,
-    secure: false,
+    secure: true,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
