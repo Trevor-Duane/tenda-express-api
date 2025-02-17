876df027f8372345376174b14ccd1331874c0873
trevor-duane@github.com <trevorkayiira@gmail.com>
2025-02-17 13:16:58 +0300

    Emailing

--- middlewares/emailTransporter.js
+++ middlewares/emailTransporter.js
@@ -6,7 +6,7 @@ dotenv.config();
 const transporter = nodemailer.createTransport({
     host: process.env.SMTP_HOST,
     port: process.env.SMTP_PORT,
-    secure: true,
+    secure: false,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
