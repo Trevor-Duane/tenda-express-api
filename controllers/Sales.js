90bdccdd4f47b5d95810762538af69f296c8d7cb
trevor-duane@github.com <trevorkayiira@gmail.com>
2025-03-12 17:57:11 +0300

    Sales Controller BUg Fix

--- controllers/Sales.js
+++ controllers/Sales.js
@@ -60,7 +60,7 @@ export const getTopSellingItems = async (req, res) => {
             { type: Sequelize.QueryTypes.SELECT }
         );
 
-        res.status(200).json({data:result});
+        res.status(200).json(result);
     } catch (error) {
         console.error("Error fetching top-selling items:", error);
         res.status(500).json({ message: "Internal Server Error" });
